581afe284942b3fc89ed754fbe6bb8710278e0d7
Aleksander Umov <cchesk@gmail.com>
2013-10-14 20:22:34 +0200

    Tried to improve animation for new comments..

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,7 +26,4 @@ DemoApp::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
-
-  # locale
-  config.i18n.available_locales = :de
 end
